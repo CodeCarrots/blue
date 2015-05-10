c6dd5f303a05435929a27a2cfa8460e86f0d58e0
soutys <soutys@go2.pl>
2015-05-10 10:15:29 +0200

    poprawka pomyłki w komunikatach

--- venvs/service/test_venv.py
+++ venvs/service/test_venv.py
@@ -4,10 +4,10 @@ import requests
 
 if __name__ == '__main__':
     if not ('1.3.4' <= pymediainfo.__version__ < '1.4.0'):
-        raise RuntimeError('Bad requests version:' \
+        raise RuntimeError('Bad pymediainfo version:' \
             ' {}'.format(pymediainfo.__version__))
 
     if not ('2.0.1' <= requests.__version__ < '2.5.0'):
-        raise RuntimeError('Bad pymediainfo version:' \
+        raise RuntimeError('Bad requests version:' \
             ' {}'.format(requests.__version__))
 
