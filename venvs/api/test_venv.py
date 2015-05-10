180ea0990b9f58faf5752d186b2f33a37dde45fa
soutys <soutys@go2.pl>
2015-05-10 10:31:56 +0200

    blue.api - update requirements.txt i testów środowiska

--- venvs/api/test_venv.py
+++ venvs/api/test_venv.py
@@ -1,8 +1,11 @@
 import django
 import requests
 import rest_framework
-from django import south
-
+try:
+    import south
+    raise RuntimeError('South module present')
+except ImportError:
+    pass
 
 if __name__ == '__main__':
     if django.VERSION < (1, 8, 0):
@@ -15,7 +18,4 @@ if __name__ == '__main__':
     if rest_framework.VERSION < '3.0.0':
         raise RuntimeError('Bad Django REST framework version:' \
             ' {}'.format(rest_framework.VERSION))
-
-    if south.__version__ < '0.9.0':
-        raise RuntimeError('Bad south version: {}'.format(south.__version__))
 
