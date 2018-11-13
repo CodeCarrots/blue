4eb99b90f67fde15cc595b3f69317d96b270caf9
soutys <soutys@go2.pl>
2018-11-13 09:11:36 +0100

    version bump

--- venvs/api/test_venv.py
+++ venvs/api/test_venv.py
@@ -1,21 +1,17 @@
 import django
 import requests
 import rest_framework
-import south
 
 
 if __name__ == '__main__':
-    if not ((1, 7, 0) <= django.VERSION < (1, 8, 0)):
+    if not ((2, 1, 0) <= django.VERSION < (3, 0, 0)):
         raise RuntimeError('Bad Django version: {}'.format(django.VERSION))
 
-    if not ('2.0.1' <= requests.__version__ < '2.5.0'):
+    if not ('2.10.1' <= requests.__version__ < '3.0.0'):
         raise RuntimeError('Bad requests version:' \
             ' {}'.format(requests.__version__))
 
-    if not ('2.3.9' <= rest_framework.VERSION < '3.0.0'):
+    if not ('2.9.0' <= rest_framework.VERSION < '4.0.0'):
         raise RuntimeError('Bad Django REST framework version:' \
             ' {}'.format(rest_framework.VERSION))
-
-    if not ('0.8.4' <= south.__version__ < '0.9.0'):
-        raise RuntimeError('Bad south version: {}'.format(south.__version__))
 
--- venvs/service/test_venv.py
+++ venvs/service/test_venv.py
@@ -3,11 +3,11 @@ import requests
 
 
 if __name__ == '__main__':
-    if not ('1.3.4' <= pymediainfo.__version__ < '1.4.0'):
+    if not ('3.0' <= pymediainfo.__version__ < '3.2'):
         raise RuntimeError('Bad pymediainfo version:' \
             ' {}'.format(pymediainfo.__version__))
 
-    if not ('2.0.1' <= requests.__version__ < '2.5.0'):
+    if not ('2.10.1' <= requests.__version__ < '3.0.0'):
         raise RuntimeError('Bad requests version:' \
             ' {}'.format(requests.__version__))
 
