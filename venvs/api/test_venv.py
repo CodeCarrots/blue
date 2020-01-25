43a9f19f69fe457c22b81165b1cf02658fa89471
soutys <soutys@go2.pl>
2020-01-25 01:05:16 +0100

    security upgrade

--- venvs/api/test_venv.py
+++ venvs/api/test_venv.py
@@ -4,14 +4,14 @@ import rest_framework
 
 
 if __name__ == '__main__':
-    if not ((2, 1, 0) <= django.VERSION < (3, 0, 0)):
+    if not ((2, 1, 0) <= django.VERSION < (4, 0, 0)):
         raise RuntimeError('Bad Django version: {}'.format(django.VERSION))
 
-    if not ('2.10.1' <= requests.__version__ < '3.0.0'):
+    if not ('2.22.0' <= requests.__version__ < '3.0.0'):
         raise RuntimeError('Bad requests version:' \
             ' {}'.format(requests.__version__))
 
-    if not ('2.9.0' <= rest_framework.VERSION < '4.0.0'):
+    if not ('3.11.0' <= rest_framework.VERSION < '4.0.0'):
         raise RuntimeError('Bad Django REST framework version:' \
             ' {}'.format(rest_framework.VERSION))
 
